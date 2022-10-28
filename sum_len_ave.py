3e0f587ee5d9f5e25f0b73a538bc4b38c80fdd50
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-10-28 19:44:20 +0300

    Adding existing project file to git

---
+++ sum_len_ave.py
@@ -0,0 +1,14 @@
+#############################################################################
+## 									   ##
+##	Title : Sum + Count + Average					   ##
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+# This is a simple program meant to receive input numbers and do a sum of the numbers, count the numbers entered and do an average.
+# Use 'done' to escape or terminate the loop after which you should see your results diplayed.
+
+print("This is a simple program meant to receive input numbers and do a sum of the numbers entered, count the numbers entered and do an average.\n \n Use 'done' to escape or terminate the program after which you should see your results diplayed.")
+print("Let's begin 
