cb6b3566a479f0e8a50413dc997f543bf633c622
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-10-28 23:44:56 +0300

    Added some changes

--- sum_len_ave.py
+++ sum_len_ave.py
@@ -10,5 +10,16 @@
 # This is a simple program meant to receive input numbers and do a sum of the numbers, count the numbers entered and do an average.
 # Use 'done' to escape or terminate the loop after which you should see your results diplayed.
 
-print("This is a simple program meant to receive input numbers and do a sum of the numbers entered, count the numbers entered and do an average.\n \n Use 'done' to escape or terminate the program after which you should see your results diplayed.")
-print("Let's begin 
+print("This is a simple program meant to receive input numbers and do a sum of the numbers entered, count the numbers entered and do an average.\n \n Use 'done' to escape or terminate the program after which you should see your results diplayed.\n Let's begin!")
+
+nun_input = input("Enter a number > ")
+
+'''
+try:
+	
+	valid_num = float(num_input)
+	
+	num_list = []
+	
+	while True:
+		if 
