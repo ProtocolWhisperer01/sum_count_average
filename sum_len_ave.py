892663fc9c4e7d9624ccc175cd3729c9c5d236a8
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-10-29 02:05:43 +0300

    Added some changes

--- sum_len_ave.py
+++ sum_len_ave.py
@@ -10,46 +10,31 @@
 # This is a simple program meant to receive input numbers and do a sum of the numbers, count the numbers entered and do an average.
 # Use 'done' to escape or terminate the loop after which you should see your results diplayed.
 
-
-
 print("\n This is a simple program meant to receive input numbers and do a sum of the numbers entered, count the numbers entered and do an average.\n\n Use 'done' to escape or terminate the program after which you should see your results diplayed.\n\n Let's begin!\n")
-
-
-
-
 		
 sum_ = 0
-	
 count = 0
+average = 0
 	
 while True:
+	
 	num_input = input("Enter a number > ")
-		
+	
 	if num_input == 'done':
-		
 		break
 	try:
-		
 		valid_num = int(num_input)
 	
 	except:
-		
 		print("Enter a valid number!")
-		
 		continue
 			
-	num_list = []
-		
+	num_list = []	
 	num_list.append(valid_num)
 		
 	for i in num_list:
-		count += 1
-			
-		sum_ += i
-			
+		count += 1	
+		sum_ += i	
 		average = sum_/count
 		
 print("Total sum is ",sum_ ,"\nTotal number entered is : ", count, "\nThe average is : ", round(average, 3))
-	
-#except:
-	#print("Kindly enter a valid input 😐️ !")
